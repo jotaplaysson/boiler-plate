87f884cd4a550c29f6f67957189cbd30217516ce
Joao Paulo Monteiro <jpmonteiro64@gmail.com>
2020-01-24 22:15:40 -0300

    Change for jest as test framework

--- src/main.js
+++
@@ -1,4 +0,0 @@
-const a = 3;
-const b = 2;
-const c = `${a} ${b}`;
-console.log(c);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: '@sucrase/webpack-loader',
+          options: {
+            transforms: ['jsx'],
+          },
+        },
+      },
+    ],
+  },
+};
